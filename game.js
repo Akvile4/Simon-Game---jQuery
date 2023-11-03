1534b8d2792c2f01a48369183ccc8bd54fe07083
Akvile4 <94171535+Akvile4@users.noreply.github.com>
2023-11-03 16:50:26 +0000

    Add files via upload

---
+++ game.js
@@ -0,0 +1,28 @@
+var gamePattern = [];
+
+var buttonColours = [
+    "red",
+    "blue",
+    "green",
+    "yellow"
+];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+    return gamePattern
+
+};
+
+$("button").click(function() {
+    alert("clicked");
+})
